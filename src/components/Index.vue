360644ddc426749c6bafc50e6a175465d24bfb4b
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-08-17 00:25:27 +0900

    feat: create index page

---
+++ src/components/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <header>
+      <img src="../assets/nagano.jpg" />
+    </header>
+    <hr />
+    <h2>ルーム一覧</h2>
+    <main>
+      <Card />
+    </main>
+  </div>
+</template>
+
+<script>
+import Card from "./Card.vue";
+
+export default {
+  components: {
+    Card,
+  },
+};
+</script>
+
+<style>
+main {
+  max-width: 800px;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+}
+</style>
