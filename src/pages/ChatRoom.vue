04db12895a5aa3cf2f8ca8da9ae1f32460e3fc6c
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-08-18 18:43:39 +0900

    fix: display loading message

--- src/pages/ChatRoom.vue
+++ src/pages/ChatRoom.vue
@@ -1,11 +1,14 @@
 <template>
   <article>
     <h2>チャットルーム</h2>
-    <h3>{{ room.name }} #{{ $route.params.id }}</h3>
-    <div v-for="(comment, index) in room.comments" :key="index">
-      <Card :title="comment.sender" :text="comment.text" />
+    <div v-if="room == null">
+      <h3>読み込み中</h3>
     </div>
-    <div>
+    <div v-else>
+      <h3>{{ room.name }} #{{ $route.params.id }}</h3>
+      <div v-for="(comment, index) in room.comments" :key="index">
+        <Card :title="comment.sender" :text="comment.text" />
+      </div>
       <router-link to="/">戻る</router-link>
     </div>
   </article>
